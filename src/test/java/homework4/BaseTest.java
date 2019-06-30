ab26d7bbb7a24c5c2f0eca401ea68f12ac99697c
Veronika Ivanova <Veronika_Ivanova@epam.com>
2019-06-30 17:46:28 +0300

    hw4 beginning

---
+++ src/test/java/homework4/BaseTest.java
@@ -0,0 +1,41 @@
+package homework4;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseTest {
+    private Properties properties;
+
+    @BeforeMethod
+    public void setUp() {
+        getProperties();
+        Configuration.browser = "CHROME";
+        Configuration.timeout = 4000;
+        Configuration.startMaximized = true;
+        open(properties.getProperty("homepage.link"));
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
+
+    public void getProperties(){
+        properties = new Properties();
+        try
+        {
+            FileInputStream fis = new FileInputStream("src/test/resources/homework3/homework3.properties");
+            properties.load(fis);
+        }
+        catch(IOException e){
+        }
+    }
+}
---
+++ src/test/java/homework4/steps/BaseSteps.java
@@ -0,0 +1,17 @@
+package homework4.steps;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class BaseSteps {
+    public void assertBrowserTitle(){
+
+    }
+
+    public void login(){
+
+    }
+
+    public void assertUsername(){
+
+    }
+}
